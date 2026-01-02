827274127dcf10d5f81f5dd72fee1084fd88943a
Fardeen <fardeenpranto7@gmail.com>
2026-01-02 13:15:34 +0600

    changes for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
